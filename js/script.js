3949aa83e7db121910ecc5dfbbc81a16c4c69f57
RJMNJ88 <90220183+RJMNJ88@users.noreply.github.com>
2022-04-17 11:33:32 -0400

    code cleanup/formatting

--- js/script.js
+++ js/script.js
@@ -14,55 +14,93 @@ closeModal.addEventListener('click', () => {
 });
 
 
-// Form submittal functionality
+// Home section contact form functionality
 var form = document.getElementById("contact-form");
     
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status.classList.add('active');
-          status.innerHTML = "<i class='fa-solid fa-check'</i>";
-          // status.innerHTML = "Thanks for your message!";
-          form.reset();
-          // form.close() // Attempt to close form
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.classList.add('active');
+      status.innerHTML = "<i class='fa-solid fa-check'</i>";
+      // status.innerHTML = "Thanks for your message!";
+      form.reset();
+      // form.close() // Attempt to close form
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
         } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status.classList.add('active');
-              status.innerHTML = "<i class='fa-solid fa-xmark'></i>";
-              alert('Could not sent message !');
-              // status.innerHTML = "Oops! There was a problem submitting your form."
-            }
-          })
+          status.classList.add('active');
+          status.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+          alert('Could not send message !');
+          // status.innerHTML = "Oops! There was a problem submitting your form."
         }
-      }).catch(error => {
-        status.classList.add('active');
-        status.innerHTML = "<i class='fa-solid fa-xmark'></i>";
-        alert('Could not sent message !');
-        // status.innerHTML = "Oops! There was a problem submitting your form."
-      });
+      })
     }
-    form.addEventListener("submit", handleSubmit)
+  }).catch(error => {
+    status.classList.add('active');
+    status.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+    alert('Could not send message !');
+    // status.innerHTML = "Oops! There was a problem submitting your form."
+  });
+}
+form.addEventListener("submit", handleSubmit)
 
-    // Remove contact form status => class="active"
-    function keyPress (e) {
-      let status = document.getElementById("status");
-      if(e.key === "Escape") {
-        status.classList.remove('active');
-      }
+// Remove contact form status => class="active"
+function keyPress (e) {
+  let status = document.getElementById("status");
+  if(e.key === "Escape") {
+    status.classList.remove('active');
+  }
+}
+
+// Contact section contact form functionality
+var form = document.getElementById("sub-contact");
     
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("sub-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
     }
+  }).then(response => {
+    if (response.ok) {
+      status.classList.add('active');
+      status.innerHTML = "<i class='fa-solid fa-check'</i>";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.classList.add('active');
+          status.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+          alert('Could not send message !');
+          // status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.classList.add('active');
+    status.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+    alert('Could not send message !');
+    // status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
 
 // Menu button functionality
 let menuBtn = document.querySelector('#menu-btn');
